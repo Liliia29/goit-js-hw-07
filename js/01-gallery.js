0e51aef547eaca6874be6faf8d70ce9b554933aa
Liliia29 <118195732+Liliia29@users.noreply.github.com>
2023-04-06 18:07:48 +0300

    Js--7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,64 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+// import '../basiclightbox/dist/basicLightbox.min.css';
+const gallery = document.querySelector('.gallery');
+
+gallery.addEventListener('click', e => {
+  e.preventDefault();
+  
+  if (e.target.classList.contains('gallery__image')) {
+    const imageSrc = e.target.dataset.source;
+    
+    const instance = basicLightbox.create(`
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+    />
+    </a>
+    </li>
+    `);
+        instance.show();
+    
+    document.addEventListener('keydown', e => {
+      if (e.key === 'Escape') {
+        instance.close();
+      }
+    });
+  }
+});
+
+
+
+
+// const containerImages = document.querySelector('.gallery');
+// const callFunctionGallery = createImagesCards(galleryItems);
+
+// containerImages.insertAdjacentHTML(
+//     'beforeend',
+//     createImagesCards(galleryItems)
+// );
+
+// function createImagesCards(arrayImages){
+//     const markup = arrayImages
+//     .map(({preview, original, description}) => {
+//         return `<li class="gallery__item">
+//         <a class="gallery__link" href="${original}">
+//           <img
+//             class="gallery__image"
+//             src="${preview}"
+//             data-source="${original}"
+//             alt="${description}"
+//           />
+//         </a>
+//       </li>`;
+//     })
+//     return
+   
+    
+// }
+// console.log(galleryItems);
