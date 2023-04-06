dbf7d2896ed446c0cdd3beb3bba008d9966bd232
Liliia29 <118195732+Liliia29@users.noreply.github.com>
2023-04-06 20:35:27 +0300

    js-7-1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const imageContainer = document.querySelector('.gallery');
+const imageMarkup = createImageCardMarkup(galleryItems);
+imageContainer.insertAdjacentHTML('beforeend', imageMarkup);
+imageContainer.addEventListener('click', onImageContainerClick);
+function createImageCardMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+                    <a class="gallery__link" href="${original}">
+                        <img
+                            class="gallery__image"
+                            src="${preview}"
+                            alt="${description}"
+                        />
+                    </a>
+                </div>`
+    }).join('');
+}
+function onImageContainerClick(evt) {
+    const isImgSwatchEl = evt.target.classList.contains('gallery__image');
+    if (!isImgSwatchEl) {
+        return;
+    }
+    evt.preventDefault();
+    const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" >
+`)
+    instance.show()
+}
+
+
 console.log(galleryItems);
